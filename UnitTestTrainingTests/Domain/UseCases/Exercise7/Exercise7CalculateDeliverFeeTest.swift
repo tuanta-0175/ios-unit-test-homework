c4f6d9a9b531c71dba4c5262836042ffa8e58977
Truong Quoc Bao <truong.quoc.bao@sun-asterisk.com>
2022-05-23 10:56:36 +0700

    Exercise 7: Deliver Fee

---
+++ UnitTestTrainingTests/Domain/UseCases/Exercise7/Exercise7CalculateDeliverFeeTest.swift
@@ -0,0 +1,80 @@
+//
+//  Exercise7CalculateDeliverFeeTest.swift
+//  UnitTestTrainingTests
+//
+//  Created by truong.quoc.bao on 23/05/2022.
+//  Copyright © 2022 Sun Asterisk. All rights reserved.
+//
+
+import XCTest
+@testable import UnitTestTraining
+
+class Exercise7CalculateDeliverFeeTest: XCTestCase, CaculatingTransportationFee {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    private func test_validateCartAmount() {
+        let result = self.validateCardAmount("100")
+        XCTAssertEqual(result.isValid, true)
+        XCTAssertEqual(result.message, "")
+    }
+    
+    private func test_premiumMember_feeGreater5000_quickDeliver() {
+        let dto = VietnamMartOrderDto(cartAmount: "5000", isPremiumMember: true, isQuickDeliver: true)
+        let result = self.calculationFee(dto: dto)
+        XCTAssertEqual(result.standardFee, 0)
+        XCTAssertEqual(result.quickFee, 500)
+    }
+    
+    private func test_premiumMember_feeLess5000_quickDeliver() {
+        let dto = VietnamMartOrderDto(cartAmount: "2000", isPremiumMember: true, isQuickDeliver: true)
+        let result = self.calculationFee(dto: dto)
+        XCTAssertEqual(result.standardFee, 0)
+        XCTAssertEqual(result.quickFee, 500)
+    }
+    
+    private func test_premiumMember_feeGreater5000_noQuickDeliver() {
+        let dto = VietnamMartOrderDto(cartAmount: "5000", isPremiumMember: true, isQuickDeliver: false)
+        let result = self.calculationFee(dto: dto)
+        XCTAssertEqual(result.standardFee, 0)
+        XCTAssertEqual(result.quickFee, 0)
+    }
+    
+    private func test_premiumMember_feeLess5000_noQuickDeliver() {
+        let dto = VietnamMartOrderDto(cartAmount: "2000", isPremiumMember: true, isQuickDeliver: false)
+        let result = self.calculationFee(dto: dto)
+        XCTAssertEqual(result.standardFee, 0)
+        XCTAssertEqual(result.quickFee, 0)
+    }
+    
+    private func test_noPremiumMember_feeGreater5000_quickDeliver() {
+        let dto = VietnamMartOrderDto(cartAmount: "5000", isPremiumMember: false, isQuickDeliver: true)
+        let result = self.calculationFee(dto: dto)
+        XCTAssertEqual(result.standardFee, 0)
+        XCTAssertEqual(result.quickFee, 500)
+    }
+    
+    private func test_noPremiumMember_feeGreater5000_noQuickDeliver() {
+        let dto = VietnamMartOrderDto(cartAmount: "5000", isPremiumMember: false, isQuickDeliver: false)
+        let result = self.calculationFee(dto: dto)
+        XCTAssertEqual(result.standardFee, 0)
+        XCTAssertEqual(result.quickFee, 0)
+    }
+    
+    private func test_noPremiumMember_feeLess5000_quickDeliver() {
+        let dto = VietnamMartOrderDto(cartAmount: "2000", isPremiumMember: false, isQuickDeliver: true)
+        let result = self.calculationFee(dto: dto)
+        XCTAssertEqual(result.standardFee, 500)
+        XCTAssertEqual(result.quickFee, 500)
+    }
+    
+    private func test_noPremiumMember_feeLess5000_noQuickDeliver() {
+        let dto = VietnamMartOrderDto(cartAmount: "2000", isPremiumMember: false, isQuickDeliver: false)
+        let result = self.calculationFee(dto: dto)
+        XCTAssertEqual(result.standardFee, 500)
+        XCTAssertEqual(result.quickFee, 0)
+    }
+    
+}
