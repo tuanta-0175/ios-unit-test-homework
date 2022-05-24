5851f03c2ef95d5364260f38a1441ec7841039c9
vutq-3033 <tran.quang.vu@sun-asterisk.com>
2022-05-24 14:33:28 +0700

    Exercise7

---
+++ UnitTestTrainingTests/Scenes/Exercise7/Exercise7NavigatorMock.swift
@@ -0,0 +1,14 @@
+//
+//  Exercise7NavigatorMock.swift
+//  UnitTestTrainingTests
+//
+//  Created by tran.quang.vu on 24/05/2022.
+//  Copyright © 2022 Sun Asterisk. All rights reserved.
+//
+
+@testable import UnitTestTraining
+import XCTest
+
+final class Exercise7NavigatorMock: Exercise7NavigatorType {
+    
+}
